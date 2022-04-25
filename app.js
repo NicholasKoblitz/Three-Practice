657299110a60fcdd63f94b5283294e8779ed57db
NicholasKoblitz <68592588+NicholasKoblitz@users.noreply.github.com>
2022-04-25 09:33:24 -0400

    Trying to find tge best methid to import three

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-
+//import * as THREE from './node_modules/three/build/three.module.js';
+const THREE = require('three')
 const canvas = document.querySelector("canvas");
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
